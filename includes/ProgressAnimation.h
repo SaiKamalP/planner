fe6d34b7576006ae50f23df0cabdd29512f57945
PAIDEMARRY SAI KAMAL <psaikamal12052005@gmail.com>
2023-12-02 18:49:14 +0530

    Moved ProgressAnimator.cpp into ProgressAnimator.h
    In c++ template function's implementations must be present in the header file itself.
    So we can't having separated .cpp file for this dosen't make sence( As one would need to include .cpp file in the code to use it, Which looks odd).

--- includes/ProgressAnimation.h
+++ includes/ProgressAnimation.h
@@ -80,3 +80,96 @@ namespace Animator{
     };
 
 } // namespace Animator
+
+
+//Implementation of the above functions
+
+namespace Animator{
+
+    // Definitions of ProgressiveFunctionInvoker functions
+
+    template<class TRet,class TObj,class TObj2> ProgressiveFunctionInvoker<TRet,TObj,TObj2>::ProgressiveFunctionInvoker(TObj &obj,TRet (TObj::*funToInvoke)(double progress),TObj2 &progressNormalizationFunObject,double (TObj2::*progressNormalizationFun)(double timeProgress),double duration,double framerate=60){
+        this->obj=&obj;
+        this->functionToInvoke=funToInvoke;
+        this->progressNormalizationFunctionObject=&progressNormalizationFunObject;
+        this->progressNormalizationFunction=progressNormalizationFun;
+        this->duration=duration;
+        this->frameRate=frameRate;
+        this->timeStep=1000/frameRate;
+        currentTime=0;
+        init();
+    }
+    
+    template<class TRet,class TObj,class TObj2>void ProgressiveFunctionInvoker<TRet,TObj,TObj2>::init(){
+        con=Glib::signal_timeout().connect(sigc::mem_fun(*this,&ProgressiveFunctionInvoker<TRet,TObj,TObj2>::onProgressTimeStep),timeStep);
+        invokeFunctionAtTimeProgress(0);
+    }
+
+    template<class TRet,class TObj,class TObj2>void ProgressiveFunctionInvoker<TRet,TObj,TObj2>::invokeFunctionAtTimeProgress(double timeProgress){
+        (obj->*functionToInvoke)((progressNormalizationFunctionObject->*progressNormalizationFunction)(0));
+    }
+
+    template<class TRet,class TObj,class TObj2>bool ProgressiveFunctionInvoker<TRet,TObj,TObj2>::onProgressTimeStep(){
+        currentTime+=timeStep;
+        if(currentTime<duration){
+            invokeFunctionAtTimeProgress(currentTime/duration);
+        }
+        else{
+            complete();
+        }
+        return true;
+    }
+    template<class TRet,class TObj,class TObj2>void ProgressiveFunctionInvoker<TRet,TObj,TObj2>::disconnect(){
+        con.disconnect();
+    }
+
+    template<class TRet,class TObj,class TObj2>void ProgressiveFunctionInvoker<TRet,TObj,TObj2>::destroy(){
+        con.disconnect();
+        delete this;
+    }
+
+    template<class TRet,class TObj,class TObj2> ProgressiveFunctionInvoker<TRet,TObj,TObj2>* ProgressiveFunctionInvoker<TRet,TObj,TObj2>::addNewProgressiveInvocation(TObj &obj,TRet (TObj::*funToInvoke)(double progress),TObj2 &progressNormalizationFunObject,double (TObj2::*progressNormalizationFun)(double timeProgress),double duration,double frameRate=60){
+        return new ProgressiveFunctionInvoker(obj,funToInvoke,progressNormalizationFunObject,progressNormalizationFun,duration,frameRate);
+    }
+
+    template<class TRet,class TObj,class TObj2> void ProgressiveFunctionInvoker<TRet,TObj,TObj2>::complete(){
+        disconnect();
+        invokeFunctionAtTimeProgress(1);
+    }
+    template<class TRet,class TObj,class TObj2> void ProgressiveFunctionInvoker<TRet,TObj,TObj2>::endWithoutComplete(){
+        disconnect();
+    }
+
+
+    // Definitions of AnimationInterface
+    template<class TRet,class TObj> AnimationInterface<TRet,TObj>::AnimationInterface(TObj &obj,TRet (TObj::*funToInvoke)(double progress),double duration,double framerate=60){
+        this->obj=&obj;
+        this->functionToInvoke=funToInvoke;
+        this->duration=duration;
+        this->framerate=framerate;
+    }
+
+    template<class TRet,class TObj> AnimationInterface<TRet,TObj>::~AnimationInterface(){
+        stop();
+    }
+    template<class TRet,class TObj> void AnimationInterface<TRet,TObj>::start(){
+        stop();
+        currentProgressiveFunctionInvoker=ProgressiveFunctionInvoker<TRet,TObj,AnimationInterface>::addNewProgressiveInvocation(*obj,functionToInvoke,*this,&AnimationInterface::progressNormalization,duration,framerate);
+    }
+    template<class TRet,class TObj> void AnimationInterface<TRet,TObj>::stop(){
+        if(currentProgressiveFunctionInvoker){
+            currentProgressiveFunctionInvoker->endWithoutComplete();
+            currentProgressiveFunctionInvoker->destroy();
+            currentProgressiveFunctionInvoker=nullptr;
+        }
+    }
+    template<class TRet,class TObj> void AnimationInterface<TRet,TObj>::complete(){
+        if(currentProgressiveFunctionInvoker){
+            currentProgressiveFunctionInvoker->complete();
+            currentProgressiveFunctionInvoker->destroy();
+            currentProgressiveFunctionInvoker=nullptr;
+        }
+    }
+
+} // namespace Animator
+
